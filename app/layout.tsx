cc4e1b6667680cd103d01b0b33a87665f99d7868
bharatpanwar4 <bharatpanwar4@gmail.com>
2024-11-11 23:05:26 +0530

    add clerk auth for protexted routes and sign in using goole and github

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/src/common/navbar";
 import Providers from "./providers";
-
+import {ClerkProvider} from '@clerk/nextjs'
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -26,6 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -37,5 +38,7 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
+    </ClerkProvider>
+
   );
 }
