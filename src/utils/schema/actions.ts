919f5d205d9c8e50aa3ec6d6d5a2988b510416e1
bharatpanwar4 <bharatpanwar4@gmail.com>
2024-11-23 13:03:52 +0530

    implemented supabase and prisma

--- src/utils/schema/actions.ts
+++ src/utils/schema/actions.ts
@@ -1,22 +1,38 @@
-'use server'
+"use server";
 
 import { profileSchema } from "./schemas";
+import db from "../db/db";
+import { clerkClient, auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+export const createProfileAction = async (
+  prevState: any,
+  formData: FormData
+) => {
+  try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please Login to create a profile");
+    console.log("uuu", user);
 
-
-export const createProfileAction = async (prevState:any,formData:FormData)=>{
-
-
-try {
-    const rawData= Object.fromEntries(formData);
-    const validatedFields=profileSchema.parse(rawData)
-    console.log(validatedFields);
-    return {message:'profile  Created'}
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+    await db.profile.create({
+      data: {
+        clerkID: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id,{
+        privateMetadata:{
+            hasProfile:true
+        }
+    })
+  
+  } catch (error) {
     
-} catch (error) {
-    console.log(error);
-    return {message:'there was an error'}
-}
-
-    
-    
-    }
+    return { message: error instanceof  Error? error.message :'An error occurred' };
+  }
+  redirect('/')
+};
